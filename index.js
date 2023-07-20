a398aba1522a378db852e95ef2befb3988910f1f
Kalsie Holt <kalsie@student.dev-island.org>
2023-07-19 21:56:24 -1000

    Edit style.css, add basic math functions to index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+let num1 = "";
+let operator = "";
+let num2 = "";
+
+function operate(x, y) {}
