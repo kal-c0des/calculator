97ada73ee04380783ad69505c383669096b1e048
Kalsie Holt <kalsie@student.dev-island.org>
2023-07-31 23:27:27 -1000

    Refactor index.html and index.js, fix delete button and calculations starting with 0

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const displayNum = document.getElementById("display");
+const displayNum = document.getElementById("display-lower");
+const displayUp = document.getElementById("display-upper");
 const numBtn = document.querySelectorAll(".number");
 const operatorBtn = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
 const clear = document.querySelector(".clear");
-const decimal = document.querySelector(".decimal");
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const divides = document.querySelector(".divs");
-const times = document.querySelector(".times");
+const deleteBtn = document.getElementById("delete");
 
 let firstArg = "";
 let operator = null;
 let secondArg = "";
-let total = "";
+let operatorClicked = false;
+let deleteClicked = false;
 
 function add(num1, num2) {
   return +num1 + +num2;
@@ -23,102 +21,97 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return +num1 * +num2;
 }
 
 function divide(num1, num2) {
   if (num2 === "0") {
     return "ERR";
   } else {
-    return num1 / num2;
+    return +num1 / +num2;
   }
 }
 
-function updateDisplay() {
-  document.getElementById("display").value = "";
-}
-
-// clear
-const clearAll = (e) => {
-  firstArg = "";
-  secondArg = "";
-  operator = "";
-  total = "";
-  displayNum.innerText = "";
-};
-
-// all the maths
-const maths = (firstArg, operator, secondArg) => {
-  let total;
-  let num1 = parseFloat(firstArg);
-  let num2 = parseFloat(secondArg);
-
-  if (operator === "+") {
-    total = add(num1, num2);
-  } else if (operator === "-") {
-    total = subtract(num1, num2);
-  } else if (operator === "*") {
-    total = multiply(num1, num2);
-  } else if (operator === "/") {
-    total = divide(num1, num2);
-  }
-  if (total.toString().length < 10) {
-    displayNum.innerText = total;
-  } else {
-    displayNum.innerText = total.toFixed(5);
-  }
-};
-
-const display = (e) => {
-  if (!operator) {
-    firstArg += e.target.textContent;
-    displayNum.innerText = firstArg;
-  } else if (firstArg && operator) {
-    secondArg += e.target.textContent;
-    displayNum.innerText = secondArg;
-  } else if (!operator && firstArg) {
-    operator += e.target.textContent;
-    displayNum.innerText = operator;
-  } else if (operator) {
-    operator = "";
-    operator += e.target.textContent;
-  }
-};
-
-const calculate = (e) => {
-  if (firstArg && secondArg && operator) {
-    if (e.target.classList.contains("equals")) {
-      maths();
-      firstArg = total;
-    } else if (e.target.classList.contains("operator")) {
-      maths();
-      firstArg = total;
-      secondArg = "";
+function calculate() {
+  if (firstArg && operator && secondArg) {
+    switch (operator) {
+      case "+":
+        firstArg = add(firstArg, secondArg);
+        break;
+      case "-":
+        firstArg = subtract(firstArg, secondArg);
+        break;
+      case "*":
+        firstArg = multiply(firstArg, secondArg);
+        break;
+      case "/":
+        firstArg = divide(firstArg, secondArg);
+        break;
     }
+    displayNum.innerText = firstArg;
+    displayUp.innerText = "";
+    operator = null;
+    secondArg = "";
   }
-};
+}
 
-// event listeners
-clear.addEventListener("click", clearAll);
-plus.addEventListener("click", calculate);
-minus.addEventListener("click", calculate);
-times.addEventListener("click", calculate);
-divides.addEventListener("click", calculate);
+function handleNumber(number) {
+  if (operator === null) {
+    firstArg += number;
+    displayNum.innerText = firstArg;
+  } else {
+    secondArg += number;
+    displayNum.innerText = secondArg;
+  }
+}
+
+function handleOperator(op) {
+  if (operator !== null) {
+    calculate();
+  }
+  operator = op;
+  displayUp.innerText = firstArg + operator;
+  displayNum.innerText = "";
+}
+
+function handleEquals() {
+  calculate();
+}
+
+function handleClear() {
+  firstArg = "";
+  operator = null;
+  secondArg = "";
+  displayNum.innerText = "";
+  displayUp.innerText = "";
+}
+
+function handleDelete() {
+  if (operator === null) {
+    // If operator is null, delete from firstArg
+    firstArg = firstArg.slice(0, -1);
+    displayNum.innerText = firstArg;
+  } else {
+    // Otherwise, delete from secondArg
+    secondArg = secondArg.slice(0, -1);
+    displayNum.innerText = secondArg;
+  }
+}
 
 numBtn.forEach((number) => {
-  number.addEventListener("click", display);
+  number.addEventListener("click", () => {
+    handleNumber(number.textContent);
+  });
 });
 
 operatorBtn.forEach((operator) => {
-  operator.addEventListener("click", display);
+  operator.addEventListener("click", () => {
+    handleOperator(operator.textContent);
+  });
 });
 
-equals.addEventListener("click", function (e) {
-  if (displayNum.value === "") {
-    displayNum.value = "";
-  } else {
-    let total = eval(displayNum.value);
-    displayNum.innerText = displayNum.value;
-    displayNum.value = total;
-  }
-});
+equals.addEventListener("click", handleEquals);
+
+clear.addEventListener("click", handleClear);
+
+deleteBtn.addEventListener("click", handleDelete);
